b004fb6a818e0ac25fbeb2bc72c5003fc9d19ee2
Frédéric FOISSEY <frederic.foissey@gmail.com>
2019-09-17 13:59:29 +0200

    vct_del fix

--- srcs/vector/ft_vct_del.c
+++ srcs/vector/ft_vct_del.c
@@ -6,7 +6,7 @@
 /*   By: ffoissey <ffoisssey@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 20:02:55 by ffoissey          #+#    #+#             */
-/*   Updated: 2019/09/01 14:16:25 by ffoissey         ###   ########.fr       */
+/*   Updated: 2019/09/17 13:59:04 by ffoissey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	vct_del(t_vector **vct)
 {
-	vct_reset(*vct, 0);
-	free(*vct);
-	*vct = NULL;
+	if (*vct != NULL)
+	{
+		ft_strdel(&((*vct)->str));
+		free(*vct);
+		*vct = NULL;
+	}
 }
