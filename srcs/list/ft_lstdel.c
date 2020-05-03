718a226ce688d88019400f52f90d652dc532a988
Frederic FOISSEY <frederic.foissey@gmail.com>
2020-05-03 17:48:26 +0200

    [UPDATE]

--- srcs/list/ft_lstdel.c
+++ srcs/list/ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: ffoissey <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 19:24:40 by ffoissey          #+#    #+#             */
-/*   Updated: 2018/11/08 19:28:44 by ffoissey         ###   ########.fr       */
+/*   Updated: 2020/05/03 16:03:49 by ffoissey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	t_list	*tmp;
 
-	if (!alst || !*alst || !del)
+	if (!alst || !*alst)
 		return ;
 	while (*alst)
 	{
 		tmp = (*alst)->next;
-		del((*alst)->content, (*alst)->content_size);
+		if (del != NULL)
+			del((*alst)->content, (*alst)->content_size);
 		free(*alst);
 		*alst = NULL;
 		*alst = tmp;
